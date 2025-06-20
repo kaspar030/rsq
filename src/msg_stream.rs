8379ca2b5522b8d3cea2e8c996129e38d21c1873
Kaspar Schleiser <kaspar@schleiser.de>
2025-06-20 10:40:25 +0200

    fix: peek into buffer for frame size
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/msg_stream.rs
+++ src/msg_stream.rs
@@ -3,7 +3,7 @@ use std::{convert::TryInto, io::ErrorKind};
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 use monoio::{
     buf::{IoBufMut, SliceMut},
-    io::{AsyncReadRent, AsyncReadRentExt, BufReader},
+    io::{AsyncBufRead, AsyncReadRent, AsyncReadRentExt, BufReader},
     BufResult,
 };
 
@@ -12,6 +12,25 @@ use crate::messaging::msg::{ChannelMsgHdr, Msg};
 pub struct FrameDecoder<IO> {
     io: BufReader<IO>,
 }
+
+// trait PeekBufReader {
+//     async fn peek(&mut self, &mut [u8]) -> std::io::Result<()>;
+// }
+
+// impl<R: AsyncReadRent> PeekBufReader for BufReader<R> {
+//     async fn peek(&mut self, target: &mut [u8]) -> std::io::Result<()> {
+//         let mut nread = 0;
+//         while nread < target.len() {
+//             let buffered = self.fill_buf().await?;
+//             let available = buffered.len();
+//             let remaining = target.len() - nread;
+//             let to_copy = available.min(remaining);
+//             target[nread..].copy_from_slice(&buffered[..to_copy]);
+//             nread += available;
+//         }
+//         Ok(())
+//     }
+// }
 
 impl<IO> FrameDecoder<IO> {
     pub fn new(io: IO) -> Self {
@@ -24,16 +43,28 @@ impl<IO> FrameDecoder<IO> {
     where
         IO: AsyncReadRent,
     {
-        let buf = BytesMut::with_capacity(8192);
-        let (res, mut buf) = self.read_exact_n(buf, 4).await;
-        let nread = match res {
-            Ok(0) => return None,
-            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => return None,
-            Err(e) => return Some(Err(e)),
-            Ok(n) => n,
-        };
+        let mut buf = BytesMut::with_capacity(128);
+        let (size, nread) = {
+            let size: u32;
+            let nread: usize;
+            let buf_content = self.io.fill_buf().await.unwrap();
+            if buf_content.len() >= size_of::<u32>() {
+                size = u32::from_be_bytes(buf_content[0..4].try_into().unwrap());
+                nread = 0;
+            } else {
+                let (res, buf_back) = self.read_exact_n(buf, 4).await;
+                buf = buf_back;
+                nread = match res {
+                    Ok(0) => return None,
+                    Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => return None,
+                    Err(e) => return Some(Err(e)),
+                    Ok(n) => n,
+                };
 
-        let size = { u32::from_be_bytes(buf[0..4].try_into().unwrap()) };
+                size = u32::from_be_bytes(buf[0..4].try_into().unwrap());
+            }
+            (size, nread)
+        };
 
         //tracing::info!("2. got size={size}");
         let whole_frame_size = size + 4;
